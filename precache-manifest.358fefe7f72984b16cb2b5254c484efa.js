c9c0217536f3f9dd4e32608913be648ef5ecf0f3
Will <rlagmlckd@gmail.com>
2019-11-02 14:25:41 -0500

    Updates

---
+++ precache-manifest.358fefe7f72984b16cb2b5254c484efa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebb1291f4e7ae2d47e91d72b94743a6",
+    "url": "/munchbot.io/index.html"
+  },
+  {
+    "revision": "15b5de8df051dd46c00c",
+    "url": "/munchbot.io/static/css/main.a1ad428b.chunk.css"
+  },
+  {
+    "revision": "8669ac6cc075662f1286",
+    "url": "/munchbot.io/static/js/2.4679efd3.chunk.js"
+  },
+  {
+    "revision": "15b5de8df051dd46c00c",
+    "url": "/munchbot.io/static/js/main.72914ee3.chunk.js"
+  },
+  {
+    "revision": "f6b81de8b81c9b3f3aef",
+    "url": "/munchbot.io/static/js/runtime-main.dfb956aa.js"
+  },
+  {
+    "revision": "7c31d983d57f6571f5987f792401e3fb",
+    "url": "/munchbot.io/static/media/food.7c31d983.gif"
+  },
+  {
+    "revision": "7c388600c74e8b564bfbb2f5d9864b9a",
+    "url": "/munchbot.io/static/media/header-background.7c388600.jpg"
+  }
+]);
